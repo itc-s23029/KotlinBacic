0825214d48dcf64981f5938d8ac51ab92177864f
s23029 <s23029@std.it-college.ac.jp>
2024-05-08 15:17:54 +0900

    4章終わり

---
+++ src/main/kotlin/Chap04.kt
@@ -0,0 +1,25 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val a: Byte = 100
+    val b: Short = 10_000
+    val c: Int = 1_000_000_000
+    val d: Long = 1_000_000_000_000_000_000L
+    val e: Float = 0.123f
+    val f: Double = 0.123_456_789
+    val g: UByte = 150U
+    val h: UInt = 22U
+    val i: UInt = 2_200_000_000U
+    val j: ULong = 1_000_000_000_000_000_000UL
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+    println(j)
+}
