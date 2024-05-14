4951c9bd72a250fde08b396af351956cc049ece1
it-s23029 <s23029@std.it-college.ac.jp>
2024-05-14 12:39:23 +0900

    13章終わり

---
+++ src/main/kotlin/Chap13.kt
@@ -0,0 +1,24 @@
+package org.example
+
+fun main(args: Array<String>) {
+    println("----13.2----")
+    val x = 5
+    for (i in 1..9) {
+        println("${x}かける${i}は、${x * i}です。")
+    }
+
+    println("----13.5----")
+    for (i in 800..1000 step 3) {
+        println("${x}かける${i}は、${x * i}です。")
+    }
+
+    println("----13.7----")
+    for (i in 9 downTo 1) {
+        println("${x}かける${i}は、${x * i}です。")
+    }
+
+    println("----13.8----")
+    for (i in 9 downTo 1 step 2) {
+        println("${x}かける${i}は、${x * i}です。")
+    }
+}
