c4503f14cb22d98b5e8dd1cd983beb9a0aeb7449
it-s23029 <s23029@std.it-college.ac.jp>
2024-05-28 14:15:40 +0900

    35章終わり

---
+++ src/main/kotlin/Chap35.kt
@@ -0,0 +1,51 @@
+package com.example.chap35
+
+fun main(args: Array<String>) {
+    run {
+        println("----35.2----")
+        val p1 = Person("タケシ", 1234)
+        val p2 = Person("ケンタ", 5678)
+        if (p1 == p2) {
+            println("二人は同一人物")
+        } else {
+            println("二人は別人")
+        }
+    }
+
+    run {
+        println("----35.3----")
+        val p1 = Person("タケシ", 1234)
+        val p2 = Person("タケシ", 1234)
+        if (p1 == p2) {
+            println("二人は同一人物")
+        } else {
+            println("二人は別人")
+        }
+    }
+
+    run {
+        println("----broke----")
+        val p1 = BrokenPerson("タケシ", 1234)
+        val p2 = BrokenPerson("タケシ", 1234)
+        if (p1 == p2) {
+            println("二人は同一人物")
+        } else {
+            println("二人は別人")
+        }
+    }
+
+    run {
+        println("----35.10----")
+        val p = Person("タケシ", 1234)
+        println(p.toString())
+
+        println("----broken2----")
+        val b = BrokenPerson("タケシ", 1234)
+        println(b.toString())
+    }
+}
+
+// 35.1
+data class Person(val name: String, val myNumber: Int)
+
+class BrokenPerson(val name: String, val myNumber: Int)
