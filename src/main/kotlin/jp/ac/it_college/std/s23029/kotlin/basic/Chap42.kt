35e554f566c432ea1eb2974bfade6399ebb39952
it-s23029 <s23029@std.it-college.ac.jp>
2024-05-28 14:17:54 +0900

    42章終わり

---
+++ src/main/kotlin/jp/ac/it_college/std/s23029/kotlin/basic/Chap42.kt
@@ -0,0 +1,10 @@
+package jp.ac.it_college.std.s23029.kotlin.basic
+
+import jp.ac.it_college.std.s23029.kotlin.basic.cars.Car
+
+fun main(args: Array<String>) {
+    val c1 = Car("赤")
+    val c2 = Car("青")
+    c1.drive()
+    c2.drive()
+}
